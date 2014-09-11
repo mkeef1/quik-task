336f9c1443c0916301e507123a7917fdab803f23
matt keef <mkeef1@gmail.com>
2014-09-11 12:28:45 -0500

    colors showing

---
+++ client/components/priority.js
@@ -0,0 +1,18 @@
+(function(){
+  'use strict';
+
+  angular.module('quiktask')
+  .factory('Priority', ['$http', function($http){
+
+    function create(priority){
+      return $http.post('/priorities', priority);
+    }
+
+    function all(){
+      return $http.get('/priorities');
+    }
+
+    return {create:create, all:all};
+  }]);
+})();
+
